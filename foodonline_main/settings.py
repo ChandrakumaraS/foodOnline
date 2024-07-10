30b1685545c9dbda2623d221580cf1f2a0fbea10
Chandru Kiran <chandruks38@gmail.com>
2024-07-10 13:44:13 +0530

    fifth commit

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STAICFILES_DIRS = [
-    'foodonline_main/static'
+    'foodonline_main/static',
 ]
 
 # Default primary key field type
