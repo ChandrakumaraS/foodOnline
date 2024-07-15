cc0cd94d414664364d574692be90f798159318ae
Chandru Kiran <chandruks38@gmail.com>
2024-07-15 11:11:41 +0530

    custom_user_model created

--- accounts/models.py
+++ accounts/models.py
@@ -47,7 +47,7 @@ class User(AbstractBaseUser):
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    user_name = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=12, blank=True)
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
