399e64d4303564241c31a51c389658e28b76a5a7
Chandru Kiran <chandruks38@gmail.com>
2024-08-01 21:38:18 +0530

    custom.js file added

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -158,3 +159,6 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL= "FoodOnline Marketplace <chandruks38@gmail.com>"
+
+
+GOOGLE_API_KEY = 'AIzaSyASByddiiLsVSBxJTqldPhSkbBwaBlvKiQ'
