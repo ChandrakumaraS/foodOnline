c43768be119e62c25ecee1e93741075236c34cf5
Chandru Kiran <chandruks38@gmail.com>
2024-08-14 17:38:25 +0530

    Dynamic tax moduke

--- foodonline_main/static/js/custom.js
+++ foodonline_main/static/js/custom.js
@@ -86,7 +86,7 @@ $(document).ready(function(){
             success: function(response){
                 console.log(response)
                 if(response.status == 'login_required'){
-                    swal(response.message, '', 'info')
+                    swal.fire(response.message, '', 'info')
                     console.log('raise the error mesaage')
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count'])
@@ -95,7 +95,7 @@ $(document).ready(function(){
                     // subtoata, tax and grandtoal
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
                 }
@@ -134,7 +134,7 @@ $(document).ready(function(){
                     // subtoata, tax and grandtoal
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
 
@@ -161,15 +161,15 @@ $(document).ready(function(){
             success: function(response){
                 console.log(response)
                 if(response.status == "Failed"){
-                    swal(response.message, "", "error")
+                    swal.fire(response.message, "", "error")
                 }else{
                     $('#cart_counter').html(response.cart_counter['cart_count'])
-                    swal(response.status, response.message, "success")
+                    swal.fire(response.status, response.message, "success")
 
                     // subtoata, tax and grandtoal
                     applyCartAmounts(
                         response.cart_amount['subtotal'],
-                        response.cart_amount['tax'],
+                        response.cart_amount['tax_dict'],
                         response.cart_amount['grand_total'],
                     )
 
@@ -202,11 +202,19 @@ $(document).ready(function(){
     }
 
     // apply cart amount
-    function applyCartAmounts(subtotal, tax, grand_total){
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
         if(window.location.pathname  == '/cart/'){
             $('#subtotal').html(subtotal)
-            $('#tax').html(tax)
             $('#total').html(grand_total)
+
+            console.log(tax_dict)
+            for(key1 in tax_dict){
+                // console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    // console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
         }
         
     }
@@ -261,7 +269,6 @@ $(document).ready(function(){
         }
     })
 
-    // REMOVE OPENING HOUR
     // REMOVE OPENING HOUR
     $(document).on('click', '.remove_hour', function(e){
         e.preventDefault();
