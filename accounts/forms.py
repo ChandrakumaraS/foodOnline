6e1e3daad9a2483444093fd1b5c16557ed4cad5a
Chandru Kiran <chandruks38@gmail.com>
2024-09-16 11:00:30 +0530

    Server related files imported

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,8 +22,8 @@ class UserProfileForm(forms.ModelForm):
     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'start typing...', 'required':'required'}))
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
-    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pincode', 'latitude', 'longitude']
